abed8c6daf842256991703ec4f4f4699658aa224
wp132331 <shrdpwjd@naver.com>
2015-09-23 12:44:32 +0900

    .

--- JavaPractice/src/org/dimigo/exception/AgeCheckException.java
+++ JavaPractice/src/org/dimigo/exception/AgeCheckException.java
@@ -15,6 +15,6 @@ public class AgeCheckException extends Exception {
 	public AgeCheckException() {
 	}
 	public AgeCheckException(Movie movie) {
-		super(movie.getTitle()+"은/는 15세 이상 관람가입니다.");
+		super(movie.getTitle()+"은/는 "+movie.getLimitAge()+"세 이상 관람가입니다.");
 	}
 }
--- JavaPractice/src/org/dimigo/exception/MovieTest.java
+++ JavaPractice/src/org/dimigo/exception/MovieTest.java
@@ -11,27 +11,28 @@ package org.dimigo.exception;
 public class MovieTest {
 
 	public static void main(String[] args) {
+		Movie[] movies = {
+				new Movie("우리는 형제입니다", 999),
+				new Movie("앤트맨", 12),
+				new Movie("사도", 12), 
+				new Movie("베테랑", 15)
+		};
+		
+		int age = 13;
+		
+		for(Movie movie : movies)
+			buyTicket(movie, age);
+	}
+	
+	private static void buyTicket(Movie movie, int age) {
 		try {
-			Movie[] movies = {
-					new Movie("앤트맨", 12),
-					new Movie("사도", 12), 
-					new Movie("베테랑", 15)
-			};
+			if(age < movie.getLimitAge())
+				throw new AgeCheckException(movie);
 			
-			int age = 13;
-			
-			for(Movie movie : movies)
-				buyTicket(movie, age);
+			System.out.println(movie.getTitle()+" 영화 재밌게 보세요.");
 			
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-	}
-	
-	private static void buyTicket(Movie movie, int age) throws AgeCheckException {
-		if(age < movie.getLimitAge())
-			throw new AgeCheckException(movie);
-		else
-			System.out.println(movie.getTitle()+" 영화 재밌게 보세요.");
 	}
 }
